fbf0b511cb831284033bbf38ef711bae7839124d
Regiane Rodrigues <107564169+Regiane-Rodrigues@users.noreply.github.com>
2023-03-08 20:30:58 -0300

    Add Arquivos
    Classe animal, exercício.

---
+++ animal/src/main/java/com/mycompany/animal/Animal.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.animal;
+
+/**
+ *
+ * @author aluno
+ */
+
+
+public class Animal {
+        
+   
+        public String nome;
+        public String tipo;
+        public String cor;
+        public char sexo;
+        public int idade;
+        
+       
+}
+
+
---
+++ animal/src/main/java/com/mycompany/animal/Cad_animal.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.animal;
+
+
+
+public class Cad_animal {
+  
+    public static void main(String[] args){
+    
+        Animal animal_1 = new Animal();
+        animal_1.nome = "Goiabinha";
+        animal_1.tipo = "Cachorro";
+        animal_1.cor = "Caramelo";
+        animal_1.sexo = 'M';
+        animal_1.idade = 2;
+        
+        Animal animal_2 = new Animal();
+        animal_2.nome = "Rogerio";
+        animal_2.tipo = "Gato";
+        animal_2.cor = "Cinza";
+        animal_2.sexo = 'M';
+        animal_2.idade = 4;
+        
+     
+        Animal animal_3 = new Animal();
+        animal_3.nome = "Rihana";
+        animal_3.tipo = "Galinha";
+        animal_3.cor = "Marrom";
+        animal_3.sexo = 'f';
+        animal_3.idade = 3;
+        
+        Animal animal_4 = new Animal();
+        animal_4.nome = "Cleber";
+        animal_4.tipo = "Cachorro";
+        animal_4.cor = "Preto";
+        animal_4.sexo = 'M';
+        animal_4.idade = 2;
+        
+        Animal animal_5 = new Animal();
+        animal_5.nome = "Ferdinando";
+        animal_5.tipo = "Cabra";
+        animal_5.cor = "Branco";
+        animal_5.sexo = 'M';
+        animal_5.idade = 7;
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+  /* private String nome;
+   private String raça;
+   private String cor;
+   private char sexo;
+   private int idade;
+   
+   public String getnome(){
+   return nome;
+   }
+   
+   public void setnome(String nome){
+   
+       this.nome = nome;
+   }
+    
+   
+   public String getraça(){
+   return nome;
+   }
+   
+   public void setnome(String nome){
+   
+       this.nome = nome;
+   }
+    
+   
+   public String getnome(){
+   return nome;
+   }
+   
+   public void setnome(String nome){
+   
+       this.nome = nome;
+   }
+    */
+   
+   
+}
