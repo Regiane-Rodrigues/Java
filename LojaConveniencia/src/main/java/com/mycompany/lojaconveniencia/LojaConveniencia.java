53c13e55b650f1cf374e28a4ce222eb54011aa4b
Regiane Rodrigues <107564169+Regiane-Rodrigues@users.noreply.github.com>
2023-03-28 17:35:18 -0300

    Teste utilizando métodos
    Acrescentado um método teste para ver se chama

--- LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/LojaConveniencia.java
+++ LojaConveniencia/src/main/java/com/mycompany/lojaconveniencia/LojaConveniencia.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.lojaconveniencia;
 
 import java.util.Scanner;
@@ -14,7 +13,7 @@ public class LojaConveniencia {
         Scanner input = new Scanner(System.in);
         
         //declarando o atributo que serve para escolher a opção do menu, ainda sem solução para caso não seja int
-        int escolha = -1;
+        int escolha = 0;
         
         System.out.println("-----------------------------------------");
         System.out.println(" || Sistema Loja de Conveniêcnia ||");
@@ -50,7 +49,7 @@ public class LojaConveniencia {
             
                 if(escolha == 1){
                 
-                    System.out.println("Teste Venda");
+                    testeVenda();
                 
                 } else if(escolha == 2){
                     System.out.println("Test compra");
@@ -60,75 +59,7 @@ public class LojaConveniencia {
                 }
                 
     } 
-        
-        
-        }
-        
-        
-        
-     /*   //início do laço de repetição do que garante que o código vai ficar repetindo a menos que a condição seja cumprida no final dele
-       do {
-            //o menu como aparecerá na tela
-            System.out.println("-----------------------------------------");
-            System.out.println("Escolha a opção desejada.");
-            System.out.println("1 - Venda");
-            System.out.println("2 - Compra");
-            System.out.println("3 - Estoque");
-            System.out.println("4 - Produto");
-            System.out.println("5 - Fiscal");
-            System.out.println("6 - Financeiro");
-            System.out.println("7 - Pessoa");
-            System.out.println("8 - Banco");
-            System.out.println("9 - Pagamento");
-            System.out.println("0 - Sair");
-            System.out.println("-----------------------------------------");
-            
-            //onde o atributo para escolha é lida e define qual opção é escolhida
-            escolha = input.hasNextInt();            
-            //o switch que escolhe a opção baseada no atributo escolha
-            switch (escolha) {
-                case 1:
-                    //um teste para entrar em outro menu dentro de um método separado no final
-                    testeVenda();
-                    break;
-                case 2:
-                    System.out.println("Teste Compras");
-                    break;
-                case 3:
-                    System.out.println("Teste Estoque");
-                    break;
-                case 4:
-                    System.out.println("Teste Cadastro Produto");
-                    break;
-                case 5:
-                    System.out.println("Teste Fiscal");
-                    break;
-                case 6:
-                    System.out.println("Teste Financeiro");
-                    break;
-                case 7:
-                    System.out.println("Teste Cadastro Pessoa");
-                    break;
-                case 8:
-                    System.out.println("Teste Cadastro Banco");
-                    break;
-                case 9:
-                    System.out.println("Teste Pagamento");
-                    break;
-                case 0:
-                    System.out.println("Obrigado Volte Sempre!");
-                    break;    
-                default:
-                    System.out.println("erro");
-                    break;
-            }
-        //a condição para o fim do laço de repetição
-        } while (escolha != 0);
-        
-    }
-    
-   /* //o teste de um método que chama outro menu que pode servir para outra classe
-    public static void testeVenda() {
+         public static void testeVenda() {
         
         Scanner ler = new Scanner(System.in);
         
@@ -141,7 +72,7 @@ public class LojaConveniencia {
             System.out.println("-----------------------------------------");
             System.out.println("Escolha a opção desejada.");
             System.out.println("1 - Teste");
-            System.out.println("0 - Voltar");
+            System.out.println("0 - Sair");
             System.out.println("-----------------------------------------");
             escolha = ler.nextInt();
             switch (escolha) {
@@ -149,7 +80,7 @@ public class LojaConveniencia {
                     System.out.println("Teste Venda");
                     break;
                 case 0:
-                    System.out.println("Voltando");
+                    System.out.println("Programa encerrado");
                     break;
                 default:
                     System.out.println("erro");
@@ -157,16 +88,7 @@ public class LojaConveniencia {
             }
         //aqui ele volta para o menu princial ao invés de encerrar o programa
         } while (escolha != 0);
-        
-        */
-        
     
-    
-
         
-        
-        
-    
-
-    
-
+         }
+}     
